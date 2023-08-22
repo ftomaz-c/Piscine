c811e6870ec03f0eafa149edaf8ac6ccade2c884
ftomaz-c <fabiocastim@gmail.com>
2023-08-22 21:29:56 +0100

    22Aug2129 c11 - ex06 & ex07

--- c/c11/ex07/ft_advanced_sort_string_tab.c
+++ c/c11/ex07/ft_advanced_sort_string_tab.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_sort_string_tab.c                               :+:      :+:    :+:   */
+/*   ft_advanced_sort_string_tab.c                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ftomaz-c <ftomaz-c@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/22 18:51:48 by ftomaz-c          #+#    #+#             */
-/*   Updated: 2023/08/22 21:27:56 by ftomaz-c         ###   ########.fr       */
+/*   Created: 2023/08/22 21:20:18 by ftomaz-c          #+#    #+#             */
+/*   Updated: 2023/08/22 21:28:58 by ftomaz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,7 @@ void	ft_swap(char **s1, char **s2)
 	*s2 = temp;
 }
 
-int		ft_strcmp(char *s1, char *s2)
-{
-	while (*s1)
-	{
-		if (*s2 > *s1)
-			break;
-		else
-			break;
-		s1++;
-		s2++;
-	}
-	return (*s1 - *s2);
-}
-
-void	ft_sort_string_tab(char **tab)
+void	ft_advanced_sort_string_tab(char **tab, int(*cmp)(char *, char *))
 {
 	int		i;
 	int		j;
@@ -46,7 +32,7 @@ void	ft_sort_string_tab(char **tab)
 		j = i + 1;
 		while (tab[j] != NULL)
 		{
-			if (ft_strcmp(tab[i], tab[j]) > 0)
+			if ((cmp(tab[i], tab[j])) > 0)
 				ft_swap(&tab[i], &tab[j]);
 			j++;
 		}
@@ -54,7 +40,21 @@ void	ft_sort_string_tab(char **tab)
 	}
 }
 
-/* #include <stdio.h>
+/* int		ft_strcmp(char *s1, char *s2)
+{
+	while (*s1)
+	{
+		if (*s2 > *s1)
+			break;
+		else
+			break;
+		s1++;
+		s2++;
+	}
+	return (*s2 - *s1);
+}
+
+#include <stdio.h>
 #define ANSI_COLOR_GREEN "\x1b[32m"
 #define ANSI_COLOR_RESET "\x1b[0m"
 
@@ -80,10 +80,11 @@ int main()
 	printf(ANSI_COLOR_GREEN "Original array:\n" ANSI_COLOR_RESET);
 	while (strings[i] != NULL)
 		printf("%s\n", strings[i++]);
-	ft_sort_string_tab(strings);
+	ft_advanced_sort_string_tab(strings, ft_strcmp);
 	i = 0;
 	printf(ANSI_COLOR_GREEN "\nSorted array:\n" ANSI_COLOR_RESET);
 	while (strings[i] != NULL)
 		printf("%s\n", strings[i++]);
 	return (0);
-} */
+}
+ */
